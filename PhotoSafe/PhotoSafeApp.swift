c1f7410fcd4a600264127791b8fbaa5d2db9e45a
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-06 01:30:25 +0400

    Photo safe

---
+++ PhotoSafe/PhotoSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSafeApp.swift
+//  PhotoSafe
+//
+//  Created by Giorgi Samkharadze on 06.09.22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
